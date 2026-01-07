20459d082845234140b55289f8f6e456c0aaec66
adeyemi.adelakun@ccsd.ac.uk <delakti@yahoo.co.uk>
2026-01-07 12:00:48 +0000

    chore: update test api to check square and env

--- api/test.js
+++ api/test.js
@@ -1,3 +1,14 @@
+import { Client, Environment } from 'square';
+
 export default function handler(req, res) {
-  res.status(200).json({ status: 'ok', message: 'API is working', nodeVersion: process.version });
+  const hasToken = !!process.env.SQUARE_ACCESS_TOKEN;
+  const squareImported = !!Client;
+  
+  res.status(200).json({ 
+      status: 'ok', 
+      message: 'API is working', 
+      nodeVersion: process.version,
+      hasSquareToken: hasToken,
+      squareImportSuccess: squareImported
+  });
 }
